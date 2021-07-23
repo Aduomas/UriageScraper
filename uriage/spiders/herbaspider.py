e9fe0e45cfb6a192a447ffc71c5ca90d415def8c
Aduomas <adomasval04@gmail.com>
2021-07-23 23:35:32 +0300

    Improved scraped data for comparison
    Uriage products' descriptions are now scraped from herba.lt and will be used for comparison between different shops.

--- uriage/spiders/herbaspider.py
+++ uriage/spiders/herbaspider.py
@@ -15,41 +15,47 @@ class HerbaSpider(scrapy.Spider):
         #         "text": product.css('a::text').get()
         #     }
 
-        # get every url of every product in a page.
-        #urlList = response.xpath('//h4[@class="product-name"]/a/@href').extract()
-        #print(urlList)
-
         item = {}
-
+        # get every url of every product in a page.
         for product in response.xpath('//h4[@class="product-name"]'):
-            product_url = product.xpath('a').attrib['href']
+            product_url = product.xpath('a/@href').get() #product.xpath('a').attrib['href']
 
-            item["title"] = product.xpath('a/text()').get()
-            item["href"] = product_url #product.xpath('a').attrib['href']
+            #item["title"] = product.xpath('a/@title').get()#product.xpath('a/text()').get()
+            #item["href"] = product_url #product.xpath('a').attrib['href']
             yield Request(product_url, callback=self.parse_product_page, meta={'item': item})
 
 
         # we now have product titles from the first page, let's scrape from another one
 
-        # css approach
-        # next_page = response.css('a.button.next.i-next').attrib['href']
+        # css approach: response.css('a.button.next.i-next').attrib['href']
 
         next_page = response.xpath('//a[@class="button next i-next"]').attrib['href']
         if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+            yield response.follow(next_page, callback=self.parse) # response.follow is basically scrapy.Request 
         
     def parse_product_page(self, response):
-        print(response.xpath('//div[@class="product-name"]/h1/text()').get())# is response.meta['item']['title'])
+        
         item = response.meta['item']
+        item['title'] = response.xpath('//div[@class="product-name"]/h1/text()').get() # title of the product, making sure it's the one we got url from.
+        item['href'] = response.request.url
+
+        # we're only getting one part of the description, because others might vary
+        # it's probably WISE to remove spaces and endlines when comparing it aswell.
+        item['description'] = response.xpath('//div[@class="std tab-description"]/p[1]/text()').get()
+        
+        # this solution would be for getting whole description, not working yet.
+        #for paragraph in response.xpath('//div[@class="std tab-description"]'):
+            #item['description'] += paragraph.xpath('text()').get()
         yield item
 
 
+class GintarineSpider(scrapy.Spider):
+    name = 'gintarine'
+    allowed_domains = ['gintarine.lt']
+    start_urls = ['https://www.gintarine.lt/search?adv=false&cid=0&mid=0&vid=0&q=URIAGE&sid=false&isc=true&orderBy=0']
 
+    def parse(self, response):
+        pass
 
-# XPATH
-# //h4[@class="product-name"]
-
-#/html/body/div[4]/div[2]/div/div[1]/div[2]/ul/li[1]/div/div[2]/h4/a
-
-# CSS
-# h4.product-name
+    def parse_product_page(self, response):
+        pass
