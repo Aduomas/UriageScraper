5fd86f14ddecf9de9b74b10fe6af5857cb2de921
Aduomas <adomasval04@gmail.com>
2021-07-24 23:09:12 +0300

    Scraping codebase completed.

--- uriage/spiders/herbaspider.py
+++ uriage/spiders/herbaspider.py
@@ -77,10 +77,53 @@ class GintarineSpider(scrapy.Spider):
 
 
 class BenuSpider(scrapy.Spider):
-    pass
+    name = 'benu'
+    allowed_domains = ['benu.lt']
+    start_urls = ['https://www.benu.lt/odos-prieziura-ir-kosmetika?vars/pageSize/all/productType/KO/brand/uriage']
+
+    def parse(self, response):
+        
+        item = {}
+        for product in response.xpath('//div[@class="productItem "]'):
+            product_url = product.xpath('div/div[1]/a/@href').get()
+            yield Request(product_url, callback=self.parse_product_page, meta={'item': item})
+
+
+    def parse_product_page(self, response):
+        item = response.meta['item']
+        item['title'] = response.xpath('//h1[@class="singleProduct__title"]/text()').get()
+        item['price'] = response.xpath('//span[@class="singleProduct__priceRegular"]/span[1]/text()').get() + '€'
+        item['description'] = response.xpath('//div[@class="singleProduct__fullInfo"]/descendant::*/text()').get() # first only
+        yield item
 
 class EuroVaistineSpider(scrapy.Spider):
-    pass
+    name = 'eurovaistine'
+    allowed_domains = ['eurovaistine.lt']
+    start_urls = ['https://www.eurovaistine.lt/paieska/rezultatai?q=uriage']
+
+    first_page = True
+
+    def parse(self, response):
+
+        item = {}
+        for product in response.xpath('//div[@class="product-item-wrapper--right"]'): #/div[2]/a/@href
+            product_url = product.xpath('div[2]/a/@href').get()
+            yield Request(product_url, callback=self.parse_product_page, meta={'item': item})
+
+        if self.first_page:
+            self.first_page = False
+            response.follow('https://www.eurovaistine.lt/paieska/rezultatai?q=uriage&lm=1&page=2', callback=self.parse)
+
+
+    def parse_product_page(self, response):
+        item = response.meta['item']
+        item['title'] = response.xpath('//h1[@itemprop="name"]/text()').get()
+        item['price'] = response.xpath('//div[@class="product-price-default"]/text()').get()
+        description = ''
+        for paragraph in response.xpath('//div[@class="info-tab-content"]/descendant::*/text()'):
+            description += paragraph.get()
+        item['description'] = description
+        yield item
 
 #configure_logging()
 #process = CrawlerProcess()
